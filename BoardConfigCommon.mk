515666c0ec1cbe3826836857a455575054acd1a2
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-09-10 20:32:42 -0300

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -168,7 +168,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_VERSION := 4.4
 TARGET_KERNEL_CONFIG := mystic-tulip-oldcam_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/tulip
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
